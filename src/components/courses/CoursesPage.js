6905761def81785ec5c5e2dc7ca6851c79d4fc55
Ray Chidaz <raychidaz@gmail.com>
2021-04-30 07:50:18 +0100

    Finish mock API set up with json server
    create tools folder with apiServer.js, mockData.js and createMockDB.js
    modify package.json scripts, start, start:dev prestart:api and start:api

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+
+import * as courseActions from '../../redux/actions/courseActions';
 
 class CoursesPage extends React.Component {
   state = {
@@ -14,7 +19,7 @@ class CoursesPage extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    alert(this.state.course.title);
+    this.props.actions.createCourse(this.state.course);
   };
 
   render() {
@@ -28,9 +33,37 @@ class CoursesPage extends React.Component {
           value={this.state.course.title}
         />
         <input type='submit' value='save' />
+        {this.props.courses.map((course) => (
+          <div key={course.title}>{course.title}</div>
+        ))}
       </form>
     );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  actions: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+
+/*mapStateToProps takes 2 arguments:- state , ownProps
+ - determines what state is passed to our component via props 
+ - be specific, request only data the component needs, 
+ - ownProps parameter lets us acces props that are being attached to this component */
+
+/* mapDispatchToProps :-this lets us decide what actions we want to expose on our component.
+ * */
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,7 +1,8 @@
 /* action creators 
  - MUST have a TYPE, payload is optional 
  */
+import * as types from './actionTypes';
 
 export function createCourse(course) {
-  return { type: 'CREATE_COURSE', course: course };
+  return { type: types.CREATE_COURSE, course: course };
 }
